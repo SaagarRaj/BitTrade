6395b3539a507716eab6398e561b701ef8beaed3
saagar <saagartiwari1992@gmail.com>
2020-08-26 11:38:06 +0530

    Firebase Auth

--- app/src/main/java/com/srt/bittrade/LoginActivity.java
+++ app/src/main/java/com/srt/bittrade/LoginActivity.java
@@ -1,14 +1,72 @@
 package com.srt.bittrade;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btn_login;
+    private TextView tv_signup, tv_forgotpass;
+    private EditText et_emailLogin, et_passLogin;
+    private String email, password;
+    private FirebaseAuth mAuth;
+    private String Email, passwod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn_login = findViewById(R.id.btn_login);
+        tv_signup = findViewById(R.id.tv_signup);
+        tv_forgotpass = findViewById(R.id.tv_forgotPass);
+        et_emailLogin = findViewById(R.id.et_emailLogin);
+        et_passLogin = findViewById(R.id.et_passLogin);
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signInWithEmailAndPassword(Email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Login successful ", LENGTH_SHORT);
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this,"Ivalid Credentials try again",Toast.LENGTH_LONG);
+                        }
+
+                    }
+                });
+            }
+        });
+
+
+        tv_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/srt/bittrade/SignupActivity.java
+++ app/src/main/java/com/srt/bittrade/SignupActivity.java
@@ -1,14 +1,52 @@
 package com.srt.bittrade;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
-
+    private EditText et_nameSignup,et_emailSignup , et_passwordSignup;
+    private Button btn_signup;
+    private FirebaseAuth mAuth;
+    private String email , pass ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        et_emailSignup = findViewById(R.id.et_emailSignup);
+        et_passwordSignup = findViewById(R.id.et_passwordSignup);
+        btn_signup = findViewById(R.id.btn_signup);
+         email = et_emailSignup.getText().toString();
+         pass = et_passwordSignup.getText().toString();
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.createUserWithEmailAndPassword(email , pass).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            startActivity(new Intent(SignupActivity.this , LoginActivity.class));
+                        }
+                        else {
+                            Toast.makeText(SignupActivity.this , "Registartion failed",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
